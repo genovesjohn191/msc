70216fc43251e03b3b018b7c83a54604c76b09ff
Arrian Pascual <arrianPas@cloudstaff.com>
2018-04-05 07:54:07 +0800

    FUSION #comment Fixed flashing issue when jwt expired

--- src/app/utilities/mcs-url.function.spec.ts
+++ src/app/utilities/mcs-url.function.spec.ts
@@ -1,4 +1,8 @@
-import { isUrlValid } from './mcs-url.function';
+import {
+  isUrlValid,
+  getHostName,
+  getDomainName
+} from './mcs-url.function';
 
 describe('Url Functions', () => {
   describe('isUrlValid()', () => {
@@ -10,6 +14,30 @@ describe('Url Functions', () => {
     it('should not valid the inputted URL', () => {
       let urlNotValid = isUrlValid('www.empty-socket.trace@');
       expect(urlNotValid).toBeFalsy();
+    });
+  });
+
+  describe('getHostName()', () => {
+    it('should return the host name of the given URL', () => {
+      let hostName = getHostName('https://lab-portal.macquariecloudservices.com');
+      expect(hostName).toBe('lab-portal.macquariecloudservices.com');
+    });
+
+    it('should return the host name of the given URL with parameters', () => {
+      let hostName = getHostName('https://portal.macquariecloudservices.com/servers?id=1234');
+      expect(hostName).toBe('portal.macquariecloudservices.com');
+    });
+  });
+
+  describe('getDomainName()', () => {
+    it('should return the domain name of the given URL', () => {
+      let hostName = getDomainName('https://lab-portal.macquariecloudservices.com');
+      expect(hostName).toBe('macquariecloudservices.com');
+    });
+
+    it('should return the domain name of the given URL with parameters', () => {
+      let hostName = getDomainName('https://portal.macquariecloudservices.com/servers?id=1234');
+      expect(hostName).toBe('macquariecloudservices.com');
     });
   });
 });
--- src/app/utilities/mcs-url.function.ts
+++ src/app/utilities/mcs-url.function.ts
@@ -1,3 +1,5 @@
+import { isNullOrEmpty } from './mcs-object.function';
+
 /**
  * Valid URL Checker
  * @param url URL string
@@ -16,4 +18,42 @@ export function isUrlValid(url: string): boolean {
 
   // Return url valid flag
   return regExpResult;
+}
+
+/**
+ * Returns the corresponding hostname from the given url
+ * @param url Url to extract the hostname
+ */
+export function getHostName(url) {
+  let match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+
+  if (!isNullOrEmpty(match) && match.length > 2
+    && typeof match[2] === 'string'
+    && match[2].length > 0) {
+    return match[2];
+  } else {
+    return null;
+  }
+}
+
+/**
+ * Returns the domain name of the given url
+ * @param url Url to extract the domain
+ */
+export function getDomainName(url) {
+  let hostName = getHostName(url);
+  let domain = hostName;
+
+  if (!isNullOrEmpty(hostName)) {
+    let parts = hostName.split('.').reverse();
+
+    if (!isNullOrEmpty(parts) && parts.length > 1) {
+      domain = parts[1] + '.' + parts[0];
+      if (hostName.toLowerCase().indexOf('.co.uk') !== -1
+        && parts.length > 2) {
+        domain = parts[2] + '.' + domain;
+      }
+    }
+  }
+  return domain;
 }
