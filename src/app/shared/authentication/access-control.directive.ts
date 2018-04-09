c63bd67869798bd71c5437f6d6339c8a478d9d2b
Arrian Pascual <arrianPas@cloudstaff.com>
2018-04-09 15:30:34 +0800

    FUSION #comment Fixed access control issues

--- src/app/shared/authentication/access-control.directive.ts
+++ src/app/shared/authentication/access-control.directive.ts
@@ -3,13 +3,10 @@ import {
   Input,
   TemplateRef,
   ViewContainerRef,
-  AfterViewInit
+  OnChanges
 } from '@angular/core';
 import { McsAccessControlService } from '../../core';
-import {
-  coerceArray,
-  refreshView
-} from '../../utilities';
+import { coerceArray } from '../../utilities';
 
 @Directive({
   selector: '[mcsAccessControl]'
@@ -21,12 +18,12 @@ import {
  * Permission: *mcsAccessControl(['VmAccess'];)
  * Feature: *mcsAccessControl([]; feature: 'experimentalFeature')
  */
-export class AccessControlDirective implements AfterViewInit {
+export class AccessControlDirective implements OnChanges {
 
   private _requiredPermission: string[];
   @Input('mcsAccessControl')
-  public set requiredPermission(requiredPermission: string[]) {
-    this._requiredPermission = coerceArray(requiredPermission);
+  public set requiredPermission(value: string[]) {
+    this._requiredPermission = coerceArray(value);
   }
 
   private _featureFlag: string;
@@ -41,15 +38,20 @@ export class AccessControlDirective implements AfterViewInit {
     public viewContainer: ViewContainerRef
   ) { }
 
-  public ngAfterViewInit() {
-    refreshView(() => {
-      let hasAccess =
-      this._accessControlService.hasAccess(this._requiredPermission, this._featureFlag);
-      if (hasAccess) {
-        this.viewContainer.createEmbeddedView(this.templateRef);
-      } else {
-        this.viewContainer.clear();
-      }
-    });
+  public ngOnChanges() {
+    // We need to put this under onChanges in order
+    // to wait for observable calls to update the DOM
+    this._removeElementByAccessControl();
+  }
+
+  /**
+   * Removes the element from the DOM based on its permissions required and feature flag
+   */
+  private _removeElementByAccessControl(): void {
+    let hasAccess = this._accessControlService
+      .hasAccess(this._requiredPermission, this._featureFlag);
+
+    hasAccess ? this.viewContainer.createEmbeddedView(this.templateRef) :
+      this.viewContainer.clear();
   }
 }
