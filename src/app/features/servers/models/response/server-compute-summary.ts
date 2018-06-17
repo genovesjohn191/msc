2dd92ba5260fe1587462a421abf2bb3dabc6a754
Leo Delos Reyes <ldelosreyes@macquarietelecom.com>
2018-06-18 07:44:51 +0800

    FUSION-1717 #comment Added generic failure fallback text

--- src/app/features/servers/models/response/server-compute-summary.ts
+++ src/app/features/servers/models/response/server-compute-summary.ts
@@ -1,3 +1,9 @@
+import {
+  McsUnitType,
+  mcsUnitTypeText
+} from '../../../../core';
+import { isNullOrEmpty } from '../../../../utilities';
+
 export class ServerComputeSummary {
   public cpuCount: number;
   public coreCount: number;
@@ -7,5 +13,23 @@ export class ServerComputeSummary {
     this.cpuCount = undefined;
     this.coreCount = undefined;
     this.memoryMB = undefined;
+  }
+
+  /**
+   * Returns cpu count with its unit
+   */
+  public get cpuCountLabel(): string {
+    return (!isNullOrEmpty(this.cpuCount) || !isNullOrEmpty(this.coreCount)) ?
+      `${this.cpuCount * this.coreCount} ${mcsUnitTypeText[McsUnitType.CPU]}` :
+      undefined;
+  }
+
+  /**
+   * Returns cpu count with its unit
+   */
+  public get memoryMBLabel(): string {
+    return (!isNullOrEmpty(this.memoryMB)) ?
+      `${this.memoryMB} ${mcsUnitTypeText[McsUnitType.Megabyte]}` :
+      undefined;
   }
 }
