0ab876b1bbce20525b3d8442a64f35e4a3c25bc3
Arrian Pascual <arrianPas@cloudstaff.com>
2019-09-26 16:50:34 +0800

    FUSION-3059 #comment Fixed the issue on dialogs and table filter selectors

--- src/app/shared/dialog/dialog-container/dialog-container.component.ts
+++ src/app/shared/dialog/dialog-container/dialog-container.component.ts
@@ -53,7 +53,7 @@ import { DialogConfig } from '../dialog-config';
 
 export class DialogContainerComponent {
 
-  @ViewChild(DialogRefDirective, { static: false })
+  @ViewChild(DialogRefDirective, { static: true })
   public portalHost: DialogRefDirective;
 
   /** Emits when an animation state changes. */
