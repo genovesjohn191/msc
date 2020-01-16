73e63f876664e48024ac7823b6d7e0828e2b1335
Raymond Sarmiento <rsarmiento@macquariecloudservices.com>
2020-01-16 10:56:52 +0800

    FUSION-3432 #comment - fix vdc scale notification header

--- src/app/core/services/mcs-notification-events.service.ts
+++ src/app/core/services/mcs-notification-events.service.ts
@@ -133,6 +133,7 @@ export class McsNotificationEventsService {
     this._jobTypeEventMap.set(JobType.ChangeServerPowerState, McsEvent.jobServerChangePowerState);
     this._jobTypeEventMap.set(JobType.ResetServerPassword, McsEvent.jobServerResetPassword);
     this._jobTypeEventMap.set(JobType.CreateResourceCatalogItem, McsEvent.jobResourceCatalogItemCreate);
+    this._jobTypeEventMap.set(JobType.ScaleVdcCompute, McsEvent.jobVdcScaleEvent);
     this._jobTypeEventMap.set(JobType.ScaleManagedServer, McsEvent.jobServerManagedScaleEvent);
     this._jobTypeEventMap.set(JobType.RaiseManagedServerInviewLevel, McsEvent.jobServerManagedRaiseInviewLevelEvent);
   }
