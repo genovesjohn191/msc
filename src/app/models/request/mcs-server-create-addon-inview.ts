b6e8ecb44aa0f2a383a3d6e6e9a628ab391f2bcc
Arrian Pascual <arrianPas@cloudstaff.com>
2020-02-06 15:14:22 +0800

    FUSION-3473 #comment Fixed issue on inview level settings in case of CVM

--- src/app/models/request/mcs-server-create-addon-inview.ts
+++ src/app/models/request/mcs-server-create-addon-inview.ts
@@ -1,6 +1,13 @@
 import { JsonProperty } from '@peerlancers/json-serialization';
+import {
+  InviewLevel,
+  InviewLevelSerialization
+} from '../enumerations/inview-level.enum';
 
 export class McsServerCreateAddOnInview {
-  @JsonProperty()
-  public inviewLevel: string = undefined;
+  @JsonProperty({
+    serializer: InviewLevelSerialization,
+    deserializer: InviewLevelSerialization
+  })
+  public inviewLevel: InviewLevel = undefined;
 }
