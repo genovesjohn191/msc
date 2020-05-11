f48d5ed7968762a9b6121e65aaef32a1b7c5243c
dgcanet <dgcanet@gmail.com>
2020-05-11 14:07:44 +0800

    FUSION-3967-fixed lint errors

--- src/app/shared/pipes/array-has-element.pipe.ts
+++ src/app/shared/pipes/array-has-element.pipe.ts
@@ -1,37 +1,37 @@
-import {
-      Pipe,
-      PipeTransform
-    } from '@angular/core';
-  import { isNullOrEmpty, isNullOrUndefined } from '@app/utilities';
-    
-  // Definition
-  const DEFAULT_KEY: string = null;
-    @Pipe({
-      name: 'mcsArrayHasElement'
-    })
-    
-    export class ArrayHasElement implements PipeTransform {
-      /**
-       * Checks the given array data if it has elements
-       * @param data Data to be transformed
-       * @param key (optional) name of a child array object that will be included on the element checking
-       */
-      public transform(data: Array<any>, key: string = DEFAULT_KEY): boolean {
-        return (this._traverseArrayElements(data, key));
-      }
-      private _traverseArrayElements(data: Array<any>, key: string): boolean {
-        let result: boolean = false;
-          if(!isNullOrEmpty(key)){
-            let pluckedObject = data.map(obj=> obj[key]);
-            let pluckedChildArray = (Array.isArray(pluckedObject)) ? pluckedObject : null;
-             result = (!isNullOrEmpty(data) && !isNullOrEmpty(pluckedChildArray));
-          }
-          else{
-            result = (!isNullOrEmpty(data));
-          }
-        
-        return result; 
-      }
-    }
-    
-  
+import {
+  Pipe,
+  PipeTransform
+} from '@angular/core';
+import {
+  isNullOrEmpty
+} from '@app/utilities';
+
+const DEFAULT_KEY: string = null;
+
+@Pipe({
+  name: 'mcsArrayHasElement'
+})
+export class ArrayHasElement implements PipeTransform {
+  /**
+   * Checks the given array data if it has elements
+   * @param data Data to be transformed
+   * @param key (optional) name of a child array object that will be included on the element checking
+   */
+  public transform(data: Array<any>, key: string = DEFAULT_KEY): boolean {
+    return (this._traverseArrayElements(data, key));
+  }
+  private _traverseArrayElements(data: Array<any>, key: string): boolean {
+    let result: boolean = false;
+
+    if (!isNullOrEmpty(key)) {
+      let pluckedObject = data.map(obj => obj[key]);
+      let pluckedChildArray = (Array.isArray(pluckedObject)) ? pluckedObject : null;
+
+      result = (!isNullOrEmpty(data) && !isNullOrEmpty(pluckedChildArray));
+    } else {
+      result = (!isNullOrEmpty(data));
+    }
+
+    return result;
+  }
+}
