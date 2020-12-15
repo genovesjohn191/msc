72e7074fdf95abbe728774361b3bbf7d9a8d58ce
WORKGROUP\ydalisay <ydalisay@macquarietelecom.com>
2020-12-15 08:41:10 +0800

    FUSION-4933 - removed crisp product type

--- src/app/features/launch-pad/core/workflows/forms/index.ts
+++ src/app/features/launch-pad/core/workflows/forms/index.ts
@@ -1,5 +1,4 @@
 export * from './provision-virtual-data-centre-vm-instance.form';
 export * from './add-on-hids.form';
 export * from './add-to-management-tools-cvm.form';
-export * from './remove-from-management-tools.form';
 export * from './form.interface';
--- src/app/features/launch-pad/core/workflows/workflow-groups/remove-from-management-tools-cvm.workflow.ts
+++ src/app/features/launch-pad/core/workflows/workflow-groups/remove-from-management-tools-cvm.workflow.ts
@@ -1,13 +1,11 @@
-import { ProductType, WorkflowType } from '@app/models';
-import { removeFromManagementToolsForm } from '../forms';
+import { WorkflowType } from '@app/models';
 import { WorkflowGroup } from '../workflow-group.interface';
 import { WorkflowConfig } from '../workflow.interface';
 
 export class RemoveFromManagementToolsCvmWorkflowGroup implements WorkflowGroup {
   public parent: WorkflowConfig = {
     id: WorkflowType.RemoveFromManagementToolsCvm,
-    crispProductType: ProductType.VirtualDataCentreVmInstance,
     title: 'Remove CVM from Management Tools',
-    form: removeFromManagementToolsForm
+    form: { config: [] }
   };
 }
