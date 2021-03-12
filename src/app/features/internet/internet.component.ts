b91d9784df074afee04f72182b8a6c2669db14ec
pbadilla <philip.c.p.badilla.hybrid@gmail.com>
2021-03-12 14:53:42 +0800

    FUSION-5185 #comment update internet ports listing page

--- src/app/features/internet/internet.component.ts
+++ src/app/features/internet/internet.component.ts
@@ -2,12 +2,17 @@ import {
   Component,
   ChangeDetectorRef,
   ChangeDetectionStrategy,
-  Injector
+  Injector,
+  ViewChild
 } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import {
-  McsTableListingBase,
-  McsNavigationService
+  McsNavigationService,
+  McsTableDataSource2,
+  McsMatTableQueryParam,
+  McsMatTableContext,
+  McsTableEvents
 } from '@app/core';
 import { McsEvent } from '@app/events';
 import { McsApiService } from '@app/services';
@@ -15,12 +20,19 @@ import {
   RouteKey,
   McsInternetPort,
   McsQueryParam,
-  McsApiCollection
+  McsFilterInfo
 } from '@app/models';
 import {
   isNullOrEmpty,
-  CommonDefinition
+  CommonDefinition,
+  getSafeProperty,
+  createObject
 } from '@app/utilities';
+import {
+  ColumnFilter,
+  Paginator,
+  Search
+} from '@app/shared';
 
 @Component({
   selector: 'mcs-internet',
@@ -28,7 +40,18 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class InternetComponent extends McsTableListingBase<McsInternetPort> {
+export class InternetComponent {
+
+  public readonly dataSource: McsTableDataSource2<McsInternetPort>;
+  public readonly dataEvents: McsTableEvents<McsInternetPort>;
+  public readonly defaultColumnFilters: McsFilterInfo[] = [
+    createObject(McsFilterInfo, { value: true, exclude: true, id: 'service' }),
+    createObject(McsFilterInfo, { value: true, exclude: false, id: 'speed' }),
+    createObject(McsFilterInfo, { value: true, exclude: false, id: 'plan' }),
+    createObject(McsFilterInfo, { value: true, exclude: false, id: 'zone' }),
+    createObject(McsFilterInfo, { value: true, exclude: false, id: 'serviceId' }),
+    createObject(McsFilterInfo, { value: true, exclude: true, id: 'action' })
+  ];
 
   public constructor(
     _injector: Injector,
@@ -36,34 +59,56 @@ export class InternetComponent extends McsTableListingBase<McsInternetPort> {
     private _navigationService: McsNavigationService,
     private _apiService: McsApiService
   ) {
-    super(_injector, _changeDetectorRef, { dataChangeEvent: McsEvent.dataChangeInternetPorts });
+    this.dataSource = new McsTableDataSource2(this._getInternetPorts.bind(this));
+    this.dataEvents = new McsTableEvents(_injector, this.dataSource, {
+      dataChangeEvent: McsEvent.dataChangeInternetPorts
+    });
+  }
+
+  @ViewChild('search')
+  public set search(value: Search) {
+    if (!isNullOrEmpty(value)) {
+      this.dataSource.registerSearch(value);
+    }
+  }
+
+  @ViewChild('paginator')
+  public set paginator(value: Paginator) {
+    if (!isNullOrEmpty(value)) {
+      this.dataSource.registerPaginator(value);
+    }
+  }
+
+  @ViewChild('columnFilter')
+  public set columnFilter(value: ColumnFilter) {
+    if (!isNullOrEmpty(value)) {
+      this.dataSource.registerColumnFilter(value);
+    }
   }
 
   public get routeKeyEnum(): typeof RouteKey {
     return RouteKey;
   }
 
-  /**
-   * Navigate to internet details page
-   * @param internet Internet Port to view the details
-   */
   public navigateToInternet(internet: McsInternetPort): void {
     if (isNullOrEmpty(internet)) { return; }
     this._navigationService.navigateTo(RouteKey.InternetDetails, [internet.id]);
   }
 
-  /**
-   * Returns the column settings key for the filter selector
-   */
-  public get columnSettingsKey(): string {
-    return CommonDefinition.FILTERSELECTOR_INTERNET_LISTING;
+
+  public retryDatasource(): void {
+    this.dataSource.refreshDataRecords();
   }
 
-  /**
-   * Gets the entity listing based on the context
-   * @param query Query to be obtained on the listing
-   */
-  protected getEntityListing(query: McsQueryParam): Observable<McsApiCollection<McsInternetPort>> {
-    return this._apiService.getInternetPorts(query);
+  private _getInternetPorts(param: McsMatTableQueryParam): Observable<McsMatTableContext<McsInternetPort>> {
+    let queryParam = new McsQueryParam();
+    queryParam.pageIndex = getSafeProperty(param, obj => obj.paginator.pageIndex);
+    queryParam.pageSize = getSafeProperty(param, obj => obj.paginator.pageSize);
+    queryParam.keyword = getSafeProperty(param, obj => obj.search.keyword);
+
+    return this._apiService.getInternetPorts(queryParam).pipe(
+      map(response => new McsMatTableContext(response?.collection,
+        response?.totalCollectionCount))
+    );
   }
 }
