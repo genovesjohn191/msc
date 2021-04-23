7294254903631942e89787fb0a5527b274bc2086
pbadilla <philip.c.p.badilla.hybrid@gmail.com>
2021-04-23 11:45:20 +0800

    FUSION-5731 #comment created base implementation for generic field components

--- src/app/shared/generic-form-fields/code-editor/code-editor-form-field.component.ts
+++ src/app/shared/generic-form-fields/code-editor/code-editor-form-field.component.ts
@@ -1,15 +1,13 @@
 import {
-  ChangeDetectorRef,
   Component,
-  EventEmitter,
   forwardRef,
-  Input,
-  Output
+  Input
 } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { McsUniqueId } from '@app/core';
 import { ContentChange } from 'ngx-quill';
+import { GenericFormFieldComponentBase } from '../generic-form-field.base';
 
 export interface CodeEditorFieldContent  {
     content: any;
@@ -31,62 +29,22 @@ export interface CodeEditorFieldContent  {
     '(blur)': 'onTouched()'
   }
 })
-export class CodeEditorFormFieldComponent implements ControlValueAccessor {
-  @Output()
-  public contentChanged: EventEmitter<string> = new EventEmitter();
-
+export class CodeEditorFormFieldComponent extends GenericFormFieldComponentBase {
   @Input()
   public id: string = McsUniqueId.NewId('code-editor');
 
   @Input()
   public height: string = '400px';
 
-  @Input()
-  public placeholder: string;
-
-  @Input()
-  public readOnly: boolean = false;
-
-  @Input()
-  public required: boolean = false;
-
-  @Input()
-  public get content(): string { return this._content; }
-  public set content(value: string) {
-    this._content = value;
-    this._changeDetectorRef.markForCheck();
-  }
-  private _content: string;
+  public backgroundColor: string = '#23241f';
+  public readOnlyBackgroundColor: string = '#2b2b2b';
 
   public quillConfig = {
     syntax: false,
     toolbar: false
   };
 
-  public constructor(private _changeDetectorRef: ChangeDetectorRef) { }
-
   public onContentChanged(param: ContentChange): void {
-    this.contentChanged.emit(param.text);
+    this.value = param.text;
   }
-
-  writeValue(obj: any): void {
-    if (obj !== this.content) {
-      this.content = obj;
-    }
-  }
-
-  public registerOnChange(fn: any): void {
-    this.propagateChange = fn;
-  }
-
-  public registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-
-  public setDisabledState?(isDisabled: boolean): void {
-  }
-
-  public propagateChange = (_: any) => {};
-
-  public onTouched = () => {};
 }
