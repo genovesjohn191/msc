ea471089b1b04652ba807bb3d157d49a0b43856e
WORKGROUP\ydalisay <ydalisay@macquarietelecom.com>
2021-07-02 11:34:09 +0800

    FUSION-6192 - Added subscription id query params in azure resources

--- src/app/models/common/mcs-azure-resource-query-params.ts
+++ src/app/models/common/mcs-azure-resource-query-params.ts
@@ -3,10 +3,12 @@ import { McsQueryParam } from './mcs-query-param';
 export class McsAzureResourceQueryParams extends McsQueryParam {
   public tagName?: string;
   public tagValue?: string;
+  public subscriptionId?: string;
 
   constructor() {
     super();
     this.tagName = '';
     this.tagValue = '';
+    this.subscriptionId = '';
   }
 }
