7e6e3323ebd7254e7331433082f5f40bd7a8e659
Arrian Pascual <apascual.development@gmail.com>
2022-07-11 16:17:00 +0000

    Merged in FUSION-8500-fix-issue-on-unit-test (pull request #2357)
    FUSION-8500 fix issue on unit test
    
    * FUSION-8500: fixed issue on unit test

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-coverage')
     ],
     client: {
       // Since we are only getting the actual result of unit in console,
