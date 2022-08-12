72cf09419ae623ac28252a74425e27930db8eba9
WORKGROUP\ydalisay <ydalisay@macquarietelecom.com>
2022-08-12 08:51:47 +0800

    FUSION-8521 - Update summary and description column limit to 60 characters

--- src/app/services/repositories/mcs-notices.repository.ts
+++ src/app/services/repositories/mcs-notices.repository.ts
@@ -6,15 +6,12 @@ import {
 import { EventBusDispatcherService } from '@app/event-bus';
 import { McsEvent } from '@app/events';
 import { McsNotice } from '@app/models';
-import { compareDates } from '@app/utilities';
 
 import { McsRepositoryBase } from '../core/mcs-repository.base';
 import { McsNoticesDataContext } from '../data-context/mcs-notices.data.context';
 
 @Injectable()
 export class McsNoticesRepository extends McsRepositoryBase<McsNotice> {
-
-  private _dispatcher: EventBusDispatcherService;
 
   constructor(_apiClientFactory: McsApiClientFactory, _eventDispatcher: EventBusDispatcherService) {
     super(
@@ -24,27 +21,5 @@ export class McsNoticesRepository extends McsRepositoryBase<McsNotice> {
         dataChangeEvent: McsEvent.dataChangeNotices
       }
     );
-
-    this._dispatcher = _eventDispatcher;
-    this._registerEvents();
-  }
-
-  /**
-   * Register the notice events
-   */
-  private _registerEvents(): void {
-    this._dispatcher.addEventListener(McsEvent.noticeReceive, this._onNoticeReceive.bind(this));
-  }
-
-  /**
-   * Event that emits when the notice has been received
-   * @param notice notice received
-   */
-  private _onNoticeReceive(notice: McsNotice): void {
-    this.addOrUpdate(notice);
-    let sortPredicate = (firstRecord: McsNotice, secondRecord: McsNotice) => {
-      return compareDates(secondRecord.createdOn, firstRecord.createdOn);
-    };
-    this.sortRecords(sortPredicate);
   }
 }
