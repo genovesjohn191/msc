85fb341f86b0802d333f1d449a41ecfceeb1ddf3
Arrian Pascual <apascual@macquarietelecom.com>
2022-11-23 07:34:48 +0800

    FUSION-9096: fixed overlapping issue on help widget

--- src/app/features-shared/help-widget/help-widget.component.ts
+++ src/app/features-shared/help-widget/help-widget.component.ts
@@ -1,17 +1,28 @@
 import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   Input,
-  ChangeDetectorRef,
-  ChangeDetectionStrategy,
   ViewEncapsulation
 } from '@angular/core';
 import {
-  animateFactory,
-  CommonDefinition,
-  isNullOrEmpty
-} from '@app/utilities';
-import { McsUniqueId, CoreRoutes } from '@app/core';
+  CoreRoutes,
+  McsUniqueId
+} from '@app/core';
 import { RouteKey } from '@app/models';
+import {
+  animateFactory,
+  isNullOrEmpty,
+  CommonDefinition
+} from '@app/utilities';
+import { ANIMATION_TIMING } from '@app/utilities/functions/animations/expansion';
 
 @Component({
   selector: 'mcs-help-widget',
@@ -20,7 +31,11 @@ import { RouteKey } from '@app/models';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
-    animateFactory.expansionVertical,
+    trigger('expansionVerticalPosition', [
+      state('collapsed', style({ width: '0px', height: '0px', visibility: 'hidden', overflow: 'hidden' })),
+      state('expanded', style({ width: '*', height: '*', visibility: 'visible', overflow: 'hidden' })),
+      transition('collapsed <=> expanded', animate(ANIMATION_TIMING))
+    ]),
     animateFactory.rotate180
   ],
   host: {
