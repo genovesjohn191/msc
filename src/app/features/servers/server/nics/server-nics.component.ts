cd2215894619ae09abd4089431be2a042a7ad192
spasaba <spasaba@MacquarieCloudServices.com>
2023-01-20 13:46:30 +1100

    FUSION-9112 - Ucs Server- NICs Tab - isESXVirtualKernelInterface updates

--- src/app/features/servers/server/nics/server-nics.component.ts
+++ src/app/features/servers/server/nics/server-nics.component.ts
@@ -488,7 +488,7 @@ export class ServerNicsComponent extends ServerDetailsBase implements OnInit, On
     if (isNullOrEmpty(nic) || !this._hasInternalPrivateCloudEngineerAccess) {
       return false;
     }
-    return this.serverIsBlade && !nic.isEsxVmkInterface;
+    return this.serverIsBlade && !nic.isESXVirtualKernelInterface;
   }
 
   /**
@@ -500,7 +500,7 @@ export class ServerNicsComponent extends ServerDetailsBase implements OnInit, On
       return false;
     }
 
-    return this.serverIsBlade && !nic.isEsxVmkInterface;
+    return this.serverIsBlade && !nic.isESXVirtualKernelInterface;
   }
 
   /**
