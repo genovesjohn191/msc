61544a3b490f54ea865dce8cd86d9db83c5dd736
spasaba <spasaba@MacquarieCloudServices.com>
2023-02-22 07:24:08 +0800

    FUSION-9347 - LaunchPad - Add optional Resource ID field to LP Provision Software Subscription workflow

--- src/app/features/launch-pad/workflows/workflow/core/forms/microsoft-software-subscription-provision.form.ts
+++ src/app/features/launch-pad/workflows/workflow/core/forms/microsoft-software-subscription-provision.form.ts
@@ -45,7 +45,7 @@ export const microsoftSoftwareSubscriptionProvisionForm: LaunchPadForm = {
       key: 'linkedService',
       value: '',
       eventName: 'linked-service-id-change',
-      dependents: ['subscription'],
+      dependents: ['subscription','resourceId'],
     }),
     new DynamicSelectAzureSubscriptionField({
       key: 'subscription',
